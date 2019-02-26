7476f54d2db68f7135ce492ab7423908f58ed638
amandat <amandatome@me.com>
2019-02-26 15:26:10 -0500

    Layout

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function () { //change to JS
-    //VARIABLES====================================================================================================================    
-    var topics = ["Star Trek", "Star Trek:TNG", "Star Trek:DS9", "Star Trek: Voyager", "Star Trek: Enterprise", "Star Trek: Discovery"];
-    console.log(topics);
+    //VARIABLES======================================================================================================================
+    var topics = ["Captain Pike", "Captain Kirk", "Captain Picard", "Captain Sisko", "Captain Janeway", "Captain Archer", "Captain Lorca"];
+    // console.log(topics);
     //MAKE BUTTONS=================================================================================================================
     function makeButtons(){
         document.querySelector("#gifButton").innerHTML = "";
@@ -24,31 +24,50 @@ $(document).ready(function () { //change to JS
     makeButtons();
     //GET GIPHY WITH API============================================================================================================
     function getGiphy() {
-        document.querySelector("#display").innerHTML = "";
-        //console.log(event);
+        console.log(event);
         var sciFi = $(this).attr("data-name");//Change to JS
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + sciFi + "&api_key=cYiJj1CIt08YVAtJrYaKhUYxYUzVn35K" + "&limit=10";
+        console.log(sciFi);
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + sciFi + "&api_key=cYiJj1CIt08YVAtJrYaKhUYxYUzVn35K" + "&limit=10"; 
         $.ajax({//change to JS fetch method
             url: queryURL,
             method: "GET"
         }).done(function (response){
-            console.log(response);
-            console.log(queryURL);
+            // console.log(response);
+            // console.log(queryURL);
+            document.querySelector("#display").innerHTML = "";
             for (var i = 0; i < response.data.length; i++) {
-                var ratings = document.createElement("p");
-                ratings.innerHTML = ("Rating: " + response.data[i].rating);
-                document.querySelector("#display").appendChild(ratings);
+                // var imageDiv = document.createElement("div");
+                var ratingsID = document.createElement("p");
+                var ratings = response.data[i].rating.toUpperCase();
+                ratingsID.innerHTML = ("RATINGS: " + ratings);
                 var imageURL = response.data[i].images.fixed_width_still.url;
+                var imageURLA = response.data[i].images.fixed_width.url;
                 var topicImage = document.createElement("img");
                 topicImage.src = imageURL;
+                topicImage.setAttribute("class", "gif");
+                topicImage.setAttribute("data-state", "still");
                 console.log(topicImage);
-                $("#display").append(topicImage);   //Change this to vanilla js
+                // document.querySelector("#display").appendChild(imageDiv);
+                document.querySelector("#display").setAttribute("class", "individualGif");
+                document.querySelector(".individualGif").appendChild(ratingsID);
+                document.querySelector(".individualGif").appendChild(topicImage);//having issues with prepend //change so there are two beside each other.
             };
         });
     };
     //ADD ON CLICK FUNCTION=================================================================================
-    $(document).on("click", ".giphy", getGiphy);
-
+   // document.addEventListener("click", ".giphy", getGiphy);
+   $(document).on("click", ".giphy", getGiphy);
+    //STILL AND ANIMATE======================================================================================
+//     function animate(){
+//         var state = $(this).attr("data-state");//js
+//         if (state === still) {
+//             console.log("yes");
+//             console.log(state);
+//         }
+       
+    
+//     }
+// animate();
 
 
 
