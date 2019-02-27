75f83f83610e879395b23056d3a62cf208765a5c
amandat <amandatome@me.com>
2019-02-27 04:01:25 -0500

    Stop animation
    Animation only working on first image

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,7 @@ $(document).ready(function () { //change to JS
     var topics = ["Jean-Luc Picard", "William Riker", "Worf", "Data", "Deanna Troi", "Beverly Crusher"];
     // console.log(topics);
     //MAKE BUTTONS=================================================================================================================
-    function makeButtons(){
+    function makeButtons() {
         document.querySelector("#gifButton").innerHTML = "";
         for (var i = 0; i < topics.length; i++) {
             var addBtn = document.createElement("button");
@@ -27,11 +27,11 @@ $(document).ready(function () { //change to JS
         //console.log(event);
         var sciFi = $(this).attr("data-name");//Change to JS
         //console.log(sciFi);
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + sciFi + "&api_key=cYiJj1CIt08YVAtJrYaKhUYxYUzVn35K" + "&limit=10"; 
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + sciFi + "&api_key=cYiJj1CIt08YVAtJrYaKhUYxYUzVn35K" + "&limit=10";
         $.ajax({//change to JS fetch method
             url: queryURL,
             method: "GET"
-        }).done(function (response){
+        }).done(function (response) {
             console.log(response);
             // console.log(queryURL);
             document.querySelector("#display").innerHTML = "";
@@ -41,47 +41,34 @@ $(document).ready(function () { //change to JS
                 var ratingsID = document.createElement("p");
                 var ratings = response.data[i].rating.toUpperCase();
                 ratingsID.innerHTML = ("RATINGS: " + ratings);
-
                 var topicImage = document.createElement("img");
                 topicImage.setAttribute("class", "gif");
-                var imageURL = ("data-still", response.data[i].images.fixed_height_still.url); 
+                var imageURL = ("data-still", response.data[i].images.fixed_height_still.url);
                 topicImage.src = imageURL;
                 topicImage.setAttribute("data-state", "still");
-                
                 var imageURLAnimate = ("data-animate", response.data[i].images.fixed_height.url);
-                // console.log(imageURLA);
-        
-                // "data-animate", response.data[i].images.fixed_height.url);
-
                 imageDiv.appendChild(ratingsID);
                 imageDiv.appendChild(topicImage);
                 document.querySelector("#display").prepend(imageDiv);
             };
-             //change to js
-    $(".gif").on("click", function(event){
-        console.log(event);
-        if ($(event.target).attr("data-state") === "still"){
-            console.log("yes");
-            topicImage.src = imageURLAnimate;
-            topicImage.setAttribute("data-state", "animate");
-          
-        } 
+            //change to js
+            $(".gif").on("click", function (event) {
+                console.log(event);
+                //use getattribute??
+                if ($(event.target).attr("data-state") === "still") {
+                    console.log("yes");
+                    topicImage.src = imageURLAnimate;
+                    topicImage.setAttribute("data-state", "animate");
+                }
+                else {
+                    topicImage.src = imageURL;
+                    topicImage.setAttribute("data-state", "still")
+                }
+            });
         });
-
-         //STILL AND ANIMATE======================================================================================
-    //console.log(this);
-   
-          
-
-    
-        // else {
-        //     $(this).attr("src", stillImage);
-        //     $(this).attr("data-state", "still");
-        // }
-    });
     };
     //ADD ON CLICK FUNCTION=================================================================================
-   // document.addEventListener("click", ".giphy", getGiphy);
-   $(document).on("click", ".giphy", getGiphy);
-  
+    // document.addEventListener("click", ".giphy", getGiphy);
+    $(document).on("click", ".giphy", getGiphy);
+
 });
