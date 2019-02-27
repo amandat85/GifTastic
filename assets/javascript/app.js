93af0ff5175f13d0902ab04f306016aec7fb084a
amandat <amandatome@me.com>
2019-02-27 12:04:50 -0500

    CSS Changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,6 @@
 $(document).ready(function () { //change to JS
     //VARIABLES======================================================================================================================
-    var topics = ["Jean-Luc Picard", "William Riker", "Worf", "Data", "Deanna Troi", "Beverly Crusher"];
-    // console.log(topics);
+    var topics = ["Captain Picard", "William Riker", "Worf", "Data", "Deanna Troi", "Beverly Crusher", "Geordi La Forge"];
     //MAKE BUTTONS=================================================================================================================
     function makeButtons() {
         document.querySelector("#gifButton").innerHTML = "";
@@ -19,28 +18,24 @@ $(document).ready(function () { //change to JS
         var giphy = $("#gifInput").val().trim();//switch to js
         topics.push(giphy);
         makeButtons();
-        //console.log(topics);  
     });
     makeButtons();
-    //GET GIPHY WITH API============================================================================================================
+    //GET GIPHY WITH API===========================================================================================================
     function getGiphy() {
-        //console.log(event);
         var sciFi = $(this).attr("data-name");//Change to JS
-        //console.log(sciFi);
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + sciFi + "&api_key=cYiJj1CIt08YVAtJrYaKhUYxYUzVn35K" + "&limit=10";
-        $.ajax({//change to JS fetch method
+        $.ajax({//change to JS fetch method? or xhr?
             url: queryURL,
             method: "GET"
         }).done(function (response) {
             console.log(response);
-            // console.log(queryURL);
             document.querySelector("#display").innerHTML = "";
             for (var i = 0; i < response.data.length; i++) {
                 var imageDiv = document.createElement("div");
                 imageDiv.setAttribute("class", "individualGif");
                 var ratingsID = document.createElement("p");
                 var ratings = response.data[i].rating.toUpperCase();
-                ratingsID.innerHTML = ("RATINGS: " + ratings);
+                ratingsID.innerHTML = ("RATING: " + ratings);
                 var topicImage = document.createElement("img");
                 topicImage.setAttribute("class", "gif");
                 var imageURL = ("data-still", response.data[i].images.fixed_height_still.url);
@@ -50,12 +45,16 @@ $(document).ready(function () { //change to JS
                 imageDiv.appendChild(ratingsID);
                 imageDiv.appendChild(topicImage);
                 document.querySelector("#display").prepend(imageDiv);
+             
             };
             //change to js
             $(".gif").on("click", function (event) {
+                var  state = $(this).attr("data-state");
+                
                 console.log(event);
                 //use getattribute??
-                if ($(event.target).attr("data-state") === "still") {
+                //assign to a specific gif?
+                if (state === "still") {
                     console.log("yes");
                     topicImage.src = imageURLAnimate;
                     topicImage.setAttribute("data-state", "animate");
@@ -64,11 +63,13 @@ $(document).ready(function () { //change to JS
                     topicImage.src = imageURL;
                     topicImage.setAttribute("data-state", "still")
                 }
+
+                console.log(state);
+            
             });
         });
     };
     //ADD ON CLICK FUNCTION=================================================================================
     // document.addEventListener("click", ".giphy", getGiphy);
     $(document).on("click", ".giphy", getGiphy);
-
 });
