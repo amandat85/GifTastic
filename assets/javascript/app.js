a2d491b08fd96e8836feb8ca1a2240cb652ff7a3
amandat <amandatome@me.com>
2019-02-26 23:19:25 -0500

    Minor Changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function () { //change to JS
     //VARIABLES======================================================================================================================
-    var topics = ["Captain Pike", "Captain Kirk", "Captain Picard", "Captain Sisko", "Captain Janeway", "Captain Archer", "Captain Lorca"];
+    var topics = ["Jean-Luc Picard", "William Riker", "Worf", "Data", "Deanna Troi", "Beverly Crusher"];
     // console.log(topics);
     //MAKE BUTTONS=================================================================================================================
     function makeButtons(){
@@ -24,31 +24,32 @@ $(document).ready(function () { //change to JS
     makeButtons();
     //GET GIPHY WITH API============================================================================================================
     function getGiphy() {
-        console.log(event);
+        //console.log(event);
         var sciFi = $(this).attr("data-name");//Change to JS
-        console.log(sciFi);
+        //console.log(sciFi);
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + sciFi + "&api_key=cYiJj1CIt08YVAtJrYaKhUYxYUzVn35K" + "&limit=10"; 
         $.ajax({//change to JS fetch method
             url: queryURL,
             method: "GET"
         }).done(function (response){
-            // console.log(response);
+            console.log(response);
             // console.log(queryURL);
             document.querySelector("#display").innerHTML = "";
             for (var i = 0; i < response.data.length; i++) {
-                // var imageDiv = document.createElement("div");
+                var imageDiv = document.createElement("div");
+                imageDiv.setAttribute("class", "individualGif");
                 var ratingsID = document.createElement("p");
                 var ratings = response.data[i].rating.toUpperCase();
                 ratingsID.innerHTML = ("RATINGS: " + ratings);
-                var imageURL = response.data[i].images.fixed_width_still.url;
-                var imageURLA = response.data[i].images.fixed_width.url;
+                var imageURL = ("data-still", response.data[i].images.fixed_height_still.url);
+                var imageURLA = ("data-animate", response.data[i].images.fixed_height.url);
+                console.log(imageURLA);
                 var topicImage = document.createElement("img");
                 topicImage.src = imageURL;
                 topicImage.setAttribute("class", "gif");
                 topicImage.setAttribute("data-state", "still");
-                console.log(topicImage);
-                // document.querySelector("#display").appendChild(imageDiv);
-                document.querySelector("#display").setAttribute("class", "individualGif");
+                //console.log(topicImage);
+                document.querySelector("#display").appendChild(imageDiv);
                 document.querySelector(".individualGif").appendChild(ratingsID);
                 document.querySelector(".individualGif").appendChild(topicImage);//having issues with prepend //change so there are two beside each other.
             };
@@ -58,28 +59,22 @@ $(document).ready(function () { //change to JS
    // document.addEventListener("click", ".giphy", getGiphy);
    $(document).on("click", ".giphy", getGiphy);
     //STILL AND ANIMATE======================================================================================
-//     function animate(){
-//         var state = $(this).attr("data-state");//js
-//         if (state === still) {
-//             console.log("yes");
-//             console.log(state);
-//         }
-       
-    
-//     }
-// animate();
+    //console.log(this);
+    //change to js
+    $("body").on("click", function(event){
+        console.log(event);
+        var state = $(this).attr("data-state");
+        var stillImage = $(this).attr("data-still");
+        var movingImage = $(this).attr("data-animate");
+        if (state === stillImage) {
+            console.log("yes");
+        };
+        // else {
+        //     $(this).attr("src", stillImage);
+        //     $(this).attr("data-state", "still");
+        // }
+    });
 
+    //on click then we need to change to animate
 
-
-
-
-
-
-
-
-
-
-
-
-
-})
+});
